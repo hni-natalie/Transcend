fd59d7cf74d2dc0367c34ab82ad6ab3b01e6cf19
rplra <lyaraazhar@gmail.com>
2026-04-09 19:53:51 +0800

    chore(cleanup): removed unused files

--- dev/services/database/init/01_extensions.sql
+++
@@ -1,7 +0,0 @@
--- Extensions
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
--- CREATE EXTENSION IF NOT EXISTS "pgvector";
-
--- -- Generate a random UUID
--- SELECT uuid_generate_v4();
--- -- Returns: '6f3e2d9c-8b4a-4e2e-9f1c-3b5a7d9e2f1a'
