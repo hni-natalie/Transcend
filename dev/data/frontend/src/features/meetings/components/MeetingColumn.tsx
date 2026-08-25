50455735b9864be1d1835d2198fc97e73d7e9564
Ho Ni Xuan (Natalie) <natalieho061@gmail.com>
2026-08-25 21:25:05 +0800

    refactor (fe-meetings): Handle responsiveness for buttons in Scheduled meetings

--- dev/data/frontend/src/features/meetings/components/MeetingColumn.tsx
+++ dev/data/frontend/src/features/meetings/components/MeetingColumn.tsx
@@ -179,7 +179,7 @@ export const MeetingColumn = ({
                   </button>
               </div>
               ) : (
-                <div className="flex gap-2">
+                <div className="flex gap-2 -ml-4">
                   <ButtonVoiceRoom 
                     className='btn-header' 
                     joinText='Start Meeting' 
