f107588c4afb92699254e27edef017915517b57a
holickka <64195032+holickka@users.noreply.github.com>
2026-09-01 17:26:10 +0800

    refactor(ui): unify Meetings attendance modal style with chat modal style

--- dev/data/frontend/src/features/livekit/components/Chat.tsx
+++ dev/data/frontend/src/features/livekit/components/Chat.tsx
@@ -1,10 +1,10 @@
 import { type ChatMessage, type ChatOptions } from '@livekit/components-core';
 import * as React from 'react';
 import { cloneSingleChild } from '../utils/utils';
-import { useMaybeLayoutContext, useChat, ChatToggle, ChatCloseIcon, ChatEntry, MessageFormatter } from '@livekit/components-react';
+import { useMaybeLayoutContext, useChat, ChatToggle, MessageFormatter } from '@livekit/components-react';
 import { meetingApi } from '@/features/meetings/api/meeting.api';
 import { useRoomContext } from '@livekit/components-react';
-import { InputTextArea } from '@/shared';
+import { IconClose, InputTextArea } from '@/shared';
 
 /** @public */
 export interface ChatProps extends React.HTMLAttributes<HTMLDivElement>, ChatOptions {
@@ -148,7 +148,7 @@ export function Chat({
           <span className="lk-chat-title">Messages</span>
           {layoutContext && (
             <ChatToggle className="lk-chat-close ml-auto">
-              <ChatCloseIcon />
+              <IconClose className="w-8 h-8 lk-attendance-close" />
             </ChatToggle>
           )}
         </div>
