d8ac4657c8993cbd9fb56173033aa44207775645
holickka <64195032+holickka@users.noreply.github.com>
2026-09-09 21:29:12 +0800

    fix(certs): fix cert container permission as need root to mount volume

--- dev/services/certs/generate_certs.sh
+++ dev/services/certs/generate_certs.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+set -e
 echo "> creating SSL certificates ..."
 
 # req			: req cert creation
@@ -20,3 +21,7 @@ if [ ! -f "/etc/certs/${DOMAIN_NAME}.crt" ]; then
 else
 	echo "> SSL certificate exists! Skipping ..."
 fi
+
+# Set proper ownership for appuser
+chown -R 1001:1001 /etc/certs
+chmod 644 /etc/certs/*
