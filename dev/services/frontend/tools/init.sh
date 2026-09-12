9294f5fe485de9a5a4d6870d7c7e44247b2a7a1f
holickka <64195032+holickka@users.noreply.github.com>
2026-09-12 15:34:39 +0800

    feat(dockerfile): upgrade to node v22 & fix npm package warning

--- dev/services/frontend/tools/init.sh
+++ dev/services/frontend/tools/init.sh
@@ -14,12 +14,23 @@ echo "> Fixing /app permissions..."
 chown -R frontend:frontend /app
 
 echo "> Installing dependencies..."
+# can remove node_modules & package.lock for clean install
+
+if [ -d node_modules ]; then
+    echo "> node_modules exists, clearing for clean install..."
+    rm -rf node_modules
+fi
+if [ -d package-lock.json ]; then
+    echo "> package-lock.json exists, clearing for clean install..."
+    rm -rf package-lock.json 
+fi
+
 su frontend -c "npm install"
 
 # echo "> [Debug] Checking if files copied..."
 # ls -la
 
-# Check ownership
+# [debug] Check ownership
 # stat -c '%u:%g %n' /app /app/node_modules /app/node_modules
 
 echo "> Creating health check flag..."
