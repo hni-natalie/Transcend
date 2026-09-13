6e08c522bdd3c25feabf1c76d8bd2acbc0550733
rplra <lyaraazhar@gmail.com>
2026-09-13 19:12:18 +0800

    chore(limiter): update to 10 reqs

--- dev/data/backend/src/routes/auth.routes.js
+++ dev/data/backend/src/routes/auth.routes.js
@@ -4,7 +4,7 @@ const { authMiddleware } = require('../middleware/auth.middleware');
 const { limiterMiddleware } = require('../middleware/limiter.middleware');
 const authController = require('../controllers/auth.controller');
 
-const authLimiter = limiterMiddleware(60 * 1000, 5, { error: '[auth] Too many attempts, try again later.' });
+const authLimiter = limiterMiddleware(60 * 1000, 10, { error: '[auth] Too many attempts, try again later.' });
 
 router.post('/login', authLimiter, authController.login);
 router.post('/google', authLimiter, authController.google);
