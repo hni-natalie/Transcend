12fdb92bb0065dba423d7767a2914cbd9b03645c
Ho Ni Xuan (Natalie) <natalieho061@gmail.com>
2026-09-24 16:13:58 +0800

    fix: SSL Cert error where failed to register ServiceWorker

--- dev/data/frontend/src/main.tsx
+++ dev/data/frontend/src/main.tsx
@@ -12,10 +12,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { registerSW } from 'virtual:pwa-register';
 import './index.css'
 import {App}from './App';
 
 const queryClient = new QueryClient();
+
+registerSW();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
